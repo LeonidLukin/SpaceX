79b07036d9e31806a9ad311560582790b5821898
Leonid Lukin <leonidlukin90@gmail.com>
2020-11-27 08:26:07 +0200

    get data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
-import Calendar from './components/Calendar/Calendar';
+import FetchData from './service/FetchData';
+// import Calendar from './components/Calendar/Calendar';
 // import Details from './components/Details/Details';
 
 // import RelaxWrapper from 'react-rellax-wrapper';
@@ -11,17 +12,54 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 
 import './style.css';
 
-class App extends React.Component {
-  
+class App extends React.Component {  
+
+  fetchData = new FetchData();
+
+  state = {
+    rocket: 'Falcon 1', 
+    rocketFeatures: null,
+    rockets: [],
+    company: null,
+  };
+
+  componentDidMount() {
+    this.updateRocket();
+    this.updateCompany();
+  }
+
+  updateRocket() {
+    this.fetchData.getRocket ()
+      .then(data => {
+        this.setState({ rockets: data.map(item => item.name) });
+        return data
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(rocketFeatures => {
+        this.setState({ rocketFeatures });
+      });
+  }
+
+  changeRocket = (rocket) => {
+    this.setState({
+      rocket
+    }, this.updateRocket);
+  }
+
+  updateCompany = () => {
+    this.fetchData.getCompany()
+    .then(company => this.setState({ company }))
+  }
+
   render() {
     return (
       <>
         <ParallaxProvider scrollAxis="vertical">
-          <Header />
-          <Main />
-          <Features />
+          <Header rockets={this.state.rockets} changeRocket={this.changeRocket}/>
+          <Main rocket={this.state.rocket} />
+          {this.state.rocketFeatures && <Features {...this.state.rocketFeatures} />}
           <Footer />
-          <Calendar />
+          {/* <Calendar /> */}
           {/* <Details /> */}
         </ParallaxProvider>
       </>
